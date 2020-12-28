e7272f45645ec173831d9c0419a90036a5d65308
Iga Hofman <iga.hofman@gmail.com>
2020-12-29 00:59:00 +0100

    Initial commi

---
+++ src/main/java/pl/hofman/projectsGmailApi/ProjectsGmailApiApplication.java
@@ -0,0 +1,14 @@
+/*package pl.hofman.projectsGmailApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsGmailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsGmailApiApplication.class, args);
+	}
+
+}
+*/
---
+++ src/test/java/pl/hofman/projectsGmailApi/ProjectsGmailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hofman.projectsGmailApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsGmailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
